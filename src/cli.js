4572e2f3a962a0f0e383a6a30cabf574a3138f9a
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-04-15 15:19:40 -0300

    add try catch and create function printList and create script to run program with npm run

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,39 @@
 import chalk from "chalk";
+import fs from "fs";
 import getFile from "./index.js";
 
-const path = process.argv[2]
+const path = process.argv;
 
-async function processText(path){
-return console.log(chalk.yellow('Links list'),await getFile(path) )
+function printList(result,identifier='') {
+  console.log(
+    chalk.yellow(`Links list`),
+    chalk.black.bgGreen(identifier),    
+    result);
 }
 
-processText(path)
+async function processText(argumentos) {
+  const way = argumentos[2];
+  try{
+    fs.lstatSync(way)
+  }catch(e){
+    if(e.code == "ENOENT"){
+        console.log('Arquivo não existe.');
+        return;
+    }
+  }
+  
+  //verify if the argument is directory or file
+  if (fs.lstatSync(way).isFile()) {
+    const result = await getFile(way);
+    printList(result);
 
+  } else if (fs.lstatSync(way).isDirectory()) {
+    const files = await fs.promises.readdir(way);
+    files.forEach(async (element) => {
+      const list = await getFile(`${way}/${element}`);
+      printList(list, element);
+    });
+  }
+}
+
+processText(path);
