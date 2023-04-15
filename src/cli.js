15216e589a257f0a0e7b36a3750354dffc14d276
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-04-15 12:37:33 -0300

    finish set up to use argv to run program

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,11 @@
 import chalk from "chalk";
 import getFile from "./index.js";
+
 const path = process.argv[2]
+
 async function processText(path){
 return console.log(chalk.yellow('Links list'),await getFile(path) )
 }
+
 processText(path)
 
