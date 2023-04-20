3e69f57c5a0161ede29863f3b3b1db2373fd3ad1
“MarcioJCosta10” <marciojcostadev@gmail.com>
2023-04-19 23:35:31 -0300

    finish

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,24 +1,40 @@
- function extractLinks(arrLinks){
-    //Object.values extract value and return an array
-    //join() extract value in array and return an string
-     return  arrLinks.map((objectLink)=> Object.values(objectLink).join())
+import chalk from "chalk";
+
+function extractLinks(arrLinks) {
+  //Object.values extract value and return an array
+  //join() extract value in array and return an string
+  return arrLinks.map((objectLink) => Object.values(objectLink).join());
 }
 
-async function checkUrl(listValidate){
-    //Promise.all() recebe uma lista de promise resolve e retornar uma lista
-    const arrStatus = await Promise
-    .all(
-        listValidate.map(async (url) => {
-            const response = await fetch(url)
-            return response.status
-        })
-    )
-    return arrStatus    
+async function checkUrl(listValidate) {
+  //Promise.all() recebe uma lista de promise resolve e retornar uma lista
+  const arrStatus = await Promise.all(
+    listValidate.map(async (url) => {
+      try {
+        const response = await fetch(url);
+        return response.status;
+      } catch (e) {
+        return manejaErros(e)
+      }
+    })
+  );
+  return arrStatus;
 }
 
-export default async function listValidate(listLinks){    
-    const links =  extractLinks(listLinks)
-    const status = await checkUrl(links)
-    return status
+function manejaErros(erro) {
+  if (erro.cause.code === "ENOTFOUND"){
+    return "link não encontrado"
+  }else{
+    return 'ocorreu um erro'
+  }
 }
 
+export default async function listValidate(listLinks) {
+  const links = extractLinks(listLinks);
+  const status = await checkUrl(links);
+  // para o mao retornar um objeto pelo map usamos os parentes englobando as chaves
+  return listLinks.map((objeto, index) => ({
+    ...objeto,
+    status: status[index],
+  }));
+}
